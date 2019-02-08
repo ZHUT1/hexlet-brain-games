cf244046f922c31512339aacd592e886dea8e29d
ZHUT <940513r@gmail.com>
2019-02-08 17:39:58 +0300

    added Prime Game

---
+++ src/games/prime.js
@@ -0,0 +1,19 @@
+import gameEngine from '..';
+import getRandom from '../utils';
+import { cons } from 'hexlet-pairs';
+
+const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  } return num > 1;
+};
+
+const generateQuestionAnswerPair = () => {
+  const question = getRandom(1, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  const data = cons(question, correctAnswer);
+  return data;
+};
+export default () => gameEngine(condition, generateQuestionAnswerPair);
