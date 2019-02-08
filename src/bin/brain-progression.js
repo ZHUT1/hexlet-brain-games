e11c1a000745efcd6e1f45f3da244338422d6a01
ZHUT <940513r@gmail.com>
2019-02-08 12:40:28 +0300

    added progression game

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import progressiongame from '../games/progression';
+
+progressiongame();
---
+++ src/games/progression.js
@@ -0,0 +1,26 @@
+import { cons, gameEngine, getRandom } from '..';
+
+const condition = 'What number is missing in the progression?';
+
+const questionAnswerPair = () => {
+  const lineLength = 10;
+  const pickNumber = getRandom(0, 10);
+  const firstNum = getRandom(0, 100);
+  const adder = getRandom(0, 100);
+  let question = '';
+  let correctAnswer;
+  let num = firstNum;
+  for (let i = 0; i < lineLength; i += 1) {
+    if (pickNumber === i) {
+      question += '.. ';
+      correctAnswer = String(num);
+      num += adder;
+    } else {
+      question += `${num} `;
+      num += adder;
+    }
+  }
+  const data = cons(question, correctAnswer);
+  return data;
+};
+export default () => gameEngine(condition, questionAnswerPair);
