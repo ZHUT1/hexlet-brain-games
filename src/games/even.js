2eeb04d579602d2e60fd01a8414389695ba8f45e
ZHUT <940513r@gmail.com>
2019-02-08 09:40:49 +0300

    fixed naming

--- src/games/even.js
+++ src/games/even.js
@@ -4,9 +4,9 @@ const condition = 'Answer "yes" if number even otherwise answer "no".';
 const isEven = num => num % 2 === 0;
 
 const questionAnswerPair = () => {
-  const randomNumber = getRandom(1, 100);
-  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-  const data = cons(randomNumber, correctAnswer);
+  const question = getRandom(1, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  const data = cons(question, correctAnswer);
   return data;
 };
 
